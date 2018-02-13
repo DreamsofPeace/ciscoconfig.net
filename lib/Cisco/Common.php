f153ac7739fa424bbb0305aa73b9273a19470f35
DreamsofPeace <31104853+DreamsofPeace@users.noreply.github.com>
2018-02-13 11:50:03 -0500

    HTTP/SSH/Line updates.

--- lib/Cisco/Common.php
+++ lib/Cisco/Common.php
@@ -44,8 +44,12 @@ abstract class Common extends Config {
         $Block->addLine("aaa authorization exec default none");
         $Block->addLine("aaa authorization commands 0 default none");
         $Block->addLine("aaa authorization commands 15 default none");
+        $this->addLine("!");
         $this->addLine("no ip http server");
+        $this->addLine("ip http authentication aaa");
+        $this->addLine("ip http secure-ciphersuite ecdhe-rsa-aes-256-cbc-sha dhe-aes-256-cbc-sha");
         $this->addLine("no ip http secure-server");
+        $this->addLine("!");
 
         $this->addOpt('FQDNHostname', 'router1.lan.local', 'string', 'Specify in FQDN format; domain name will also be used for DHCP scopes');
         $this->addOpt('AdminUsername', 'admin', 'string', 'This user will be created with privilege level 15');
@@ -83,6 +87,11 @@ abstract class Common extends Config {
         
         $Line = $this->addBlock("line con 0", ConfBlock::POS_LINE);
         $Line->addLine('logging synchronous');
+        $Line->addLine("exec prompt timestamp");
+        $Line->addLine("exec-timeout 15 0");
+        $Line->addLine("!The Next Line will change the default break character to Ctrl+C");
+        $Line->addLine("escape-character 3");
+        $Line->addLine("privilege level 15");
         
     }
     
@@ -94,6 +103,15 @@ abstract class Common extends Config {
         $this->addLine("!  / !! \  crypto key generate rsa general-keys modulus 4096");
         $this->addLine("! <------>  ");
         $this->addLine("ip ssh version 2");
+        $this->addLine("ip ssh logging events");
+        $this->addLine("ip ssh dh min size 4096");
+        $this->addLine("ip ssh dscp 16");
+        $this->addLine("ip ssh rekey time 30");
+        $this->addLine("ip ssh server algorithm mac hmac-sha1");
+        $this->addLine("ip ssh server algorithm encryption aes256-ctr aes256-cbc");
+        $this->addLine("ip ssh client algorithm mac hmac-sha1");
+        $this->addLine("ip ssh client algorithm encryption aes256-ctr aes256-cbc");
+
         if ($Port != 22 && $Port != 0) {
             $this->addLine("ip ssh port {$Port} rotary 1");
         }
@@ -106,7 +124,10 @@ abstract class Common extends Config {
         $Block->addLine("logging synchronous");
         $Block->addLine("transport input ssh");
         $Block->addLine("exec prompt timestamp");
+        $Block->addLine("exec-timeout 15 0");
+        $Block->addLine("!The Next Line will change the default break character to Ctrl+C");
         $Block->addLine("escape-character 3");
+        $Block->addLine("privilege level 15");
         if ($Port != 22 && $Port != 0) {
             $Block->addLine("rotary 1");
         }
